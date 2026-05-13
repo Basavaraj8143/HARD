46d9e937b797758130d89662529f7c2eed077be6
Basavaraj <basavarajningasani123@gmail.com>
2026-05-13 19:47:55 +0530

    practice arena and list of problems

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// practicearean.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*#include <iostream>
+using namespace std;
+int main(){
+ for(int i=0;i<5;i++){
+            if(i==2)break;
+
+    for(int j=0;j<3;j++){
+        cout<< "star"<< endl;
+
+    }
+ }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+  int x=2;
+  int y=5;
+  for(int i=1;i<4;i++){
+    x=x+y;
+    y=x-y;
+  }
+  cout<<"values are:&x,&y" <<x<<y;
+}
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "hello" << endl;
+}
